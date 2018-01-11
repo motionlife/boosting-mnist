b2d594063b476b7ab12d47048532974a05fe6dc5
Hao Xiong <haoiong@outlook.com>
2018-01-11 00:28:02 -0600

    For Azure 64 cores

--- src/main/java/CIFAR.java
+++ src/main/java/CIFAR.java
@@ -15,7 +15,7 @@ public class CIFAR {
 
     public static void main(String args[]) throws IOException {
         String path = System.getProperty("os.name").toLowerCase().contains("win") ?
-                "C:/Users/HaoXiong/IdeaProjects/ChowLiuBoosting" : "/people/cs/h/hxx160130/boosting";
+                "C:/Users/HaoXiong/IdeaProjects/ChowLiuBoosting" : System.getProperty("user.dir");
         WeightedData[] dataset = new WeightedData[50000];
         int[][] test = new int[10000][1025];
 
